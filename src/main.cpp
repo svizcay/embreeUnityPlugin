8eebe09262fd187227239f422da4ad675cbcf698
Sebastian Vizcay <sebastian.vizcay@inria.fr>
2017-09-07 19:05:19 +0200

    working in unity plugin

--- src/main.cpp
+++ src/main.cpp
@@ -31,15 +31,42 @@ int main(int argc, char * argv[])
     int numTriangles = 2;
     int numVertices = 4;
 	//unsigned proxyMeshGeomId = rtcNewTriangleMehs2(rtcScene, RTC_GEOMETRY_STATIC, numTriangles, numVertices, 1);
-	//unsigned proxyMeshGeomId = rtcNewTriangleMesh(rtcScene, geomFlags, nrTriangles, nrVertices);
+	unsigned proxyMeshGeomId = rtcNewTriangleMesh(rtcScene, RTC_GEOMETRY_STATIC, numTriangles, numVertices);
 
-	//Vertex* vertices = (Vertex*)rtcMapBuffer(rtcScene, proxyMeshGeomId, RTC_VERTEX_BUFFER);
-	// fill vertices here
-	//rtcUnmapBuffer(rtcScene, proxyMeshGeomId, RTC_VERTEX_BUFFER);
+	Vertex* vertices = (Vertex*)rtcMapBuffer(rtcScene, proxyMeshGeomId, RTC_VERTEX_BUFFER);
+	// remember that unity uses clockwise triangle order
+	vertices[0].x = -10; vertices[0].y = -2; vertices[0].z = -10;
+	vertices[1].x = -10; vertices[1].y = -2; vertices[1].z = +10;
+	vertices[2].x = +10; vertices[2].y = -2; vertices[2].z = -10;
+	vertices[3].x = +10; vertices[3].y = -2; vertices[3].z = +10;
+	rtcUnmapBuffer(rtcScene, proxyMeshGeomId, RTC_VERTEX_BUFFER);
 
-	//Triangle* triangles = (Triangle*)rtcMapBuffer(rtcScene, proxyMeshGeomId, RTC_INDEX_BUFFER);
-	// fill triangle indices here
-	//rtcUnmapBuffer(rtcScene, proxyMeshGeomId, RTC_INDEX_BUFFER);
+	Triangle* triangles = (Triangle*)rtcMapBuffer(rtcScene, proxyMeshGeomId, RTC_INDEX_BUFFER);
+	triangles[0].v0 = 0; triangles[0].v1 = 2; triangles[0].v2 = 1;
+	triangles[1].v0 = 1; triangles[1].v1 = 2; triangles[1].v2 = 3;
+	rtcUnmapBuffer(rtcScene, proxyMeshGeomId, RTC_INDEX_BUFFER);
+
+	// commit changes to the scene (all buffers have to get unmapped before commiting)
+	rtcCommit(rtcScene);
+
+
+	// throw ray
+	RTCRay ray;
+	ray.org[0] = 0;
+	ray.org[1] = 0;
+	ray.org[2] = 0;
+	ray.dir[0] = 0;
+	ray.dir[1] = -1;
+	ray.dir[2] = 0;
+
+	ray.tnear = 0;
+	ray.tfar = std::numeric_limits<float>::infinity();
+	ray.geomID = RTC_INVALID_GEOMETRY_ID;
+
+	rtcIntersect(rtcScene, ray);
+
+	cout << "distance : " << ray.tfar << endl;
+
 
 	rtcDeleteScene(rtcScene);
 
